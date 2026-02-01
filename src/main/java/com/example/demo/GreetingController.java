6e40dd6980fc2fdf33e3b260c97d4f6ec666fbfc
Selith <selithrubasingha@gmail.com>
2026-02-01 16:39:16 +0530

    building a super simple Rest Api

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+import com.example.demo.dto.Greeting;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+
+@SpringBootApplication
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(),template.formatted(name));
+    }
+
+
+
+}
---
+++ src/main/java/com/example/demo/dto/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.demo.dto;
+
+public record Greeting(long id, String content) { }
