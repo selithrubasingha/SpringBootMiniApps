c5c6e41bc6116f5f3e7708db96f09f6efe96dea7
Selith <selithrubasingha@gmail.com>
2026-02-04 18:51:36 +0530

    repository annotation using

--- src/main/java/com/example/demo/OwnerDemo/OwnerRepository.java
+++ src/main/java/com/example/demo/OwnerDemo/OwnerRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.OwnerDemo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends JpaRepository<Owner,Integer> {
 }
