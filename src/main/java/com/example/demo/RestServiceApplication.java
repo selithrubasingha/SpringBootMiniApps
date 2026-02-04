9f9d56b2e42cb29e315c79b28541dea576dc5e9d
Selith <selithrubasingha@gmail.com>
2026-02-04 20:15:58 +0530

    added database starting commands

--- src/main/java/com/example/demo/RestServiceApplication.java
+++ src/main/java/com/example/demo/RestServiceApplication.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//remember to run the database!!
+//sudo systemctl start mysql
 @SpringBootApplication
 public class RestServiceApplication {
 
